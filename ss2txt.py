ad0e63185fa54e4a67a4130e27284b2a8921a421
s0ulix <54552051+s0ulix@users.noreply.github.com>
2021-05-04 12:36:03 +0530

    Added the feature of selecting a new file

--- ss2txt.py
+++ ss2txt.py
@@ -1,84 +1,88 @@
 import webbrowser
 import easyocr
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
+from tkinter.filedialog import askopenfile
 def clr():
     global out
     out.delete('1.0', END)
-def textg():
+def textg(ch):
     clr()
     try:
         global reader
         global out
-        img = ImageGrab.grabclipboard()
-        img.save('snip.png', 'PNG')
-        file_name = "snip.png"
+        if(ch==1):
+            img = ImageGrab.grabclipboard()
+            img.save('snip.png', 'PNG')
+            file_name = "snip.png"
+        else:
+            file_name=ch
         output = reader.readtext(file_name)
         b = ''
         for a in output:
             b += a[1]
             b += ' '
-        g(b)
+        return b
     except:
         messagebox.showerror(title="Error", message="No screenshot taken or copied image")
-def textp():
-    clr()
-    try:
-        global reader
-        global out
-        img = ImageGrab.grabclipboard()
-        img.save('snip.png', 'PNG')
-        file_name = "snip.png"
-        output = reader.readtext(file_name)
-        b = ''
-        for a in output:
-            b += a[1]
-            b += ' '
-        p(b)
-    except:
-        messagebox.showerror(title="Error", message="No screenshot taken or copied image")
-def texts():
-    clr()
-    try:
-        global reader
-        global out
-        img = ImageGrab.grabclipboard()
-        img.save('snip.png', 'PNG')
-        file_name = "snip.png"
-        output = reader.readtext(file_name)
-        b = ''
-        for a in output:
-            b += a[1]
-            b += ' '
-        s(b)
-    except:
-        messagebox.showerror(title="Error", message="No screenshot taken or copied image")
-def g(b):
-    out.insert(INSERT, b)
-    s(b)
-    p(b)
-def s(b):
-    url = "https://www.google.com.tr/search?q={}".format(b)
-    webbrowser.open_new_tab(url)
-    out.insert(INSERT, b)
-def p(b):
-    pyperclip.copy(b)
-    pyperclip.paste()
-    out.insert(INSERT, b)
+        return None
+def fi():
+    file = askopenfile(mode='r',filetypes=[("image", ".jpeg"),("image", ".png"),("image", ".jpg"),])
+    a=str(file).split()
+    b=textg(a[1][6:-1])
+    return b
+def cpysr(b):
+    if(b!=None):
+        out.insert(INSERT, b)
+        url = "https://www.google.com.tr/search?q={}".format(b)
+        webbrowser.open_new_tab(url)
+        pyperclip.copy(b)
+        pyperclip.paste()
+def ser(b):
+    if (b != None):
+        url = "https://www.google.com.tr/search?q={}".format(b)
+        webbrowser.open_new_tab(url)
+        out.insert(INSERT, b)
+def cpy(b):
+    if (b != None):
+        pyperclip.copy(b)
+        pyperclip.paste()
+        out.insert(INSERT, b)
+def p1():
+    b=textg(1)
+    cpysr(b)
+def p2():
+    b = textg(1)
+    ser(b)
+def p3():
+    b = textg(1)
+    cpy(b)
+def s1():
+    b=fi()
+    cpysr(b)
+def s2():
+    b=fi()
+    ser(b)
+def s3():
+    b=fi()
+    cpy(b)
 global reader
 reader = easyocr.Reader(['en'])
 root = Tk()
 logo = PhotoImage(file='logo.png')
 root.configure(bg="black")
-root.geometry("400x350")
+root.geometry("400x450")
 root.title("Text Extracter")
 root.iconphoto(False,logo)
 Label(text="By s0ulix", bg="red", width="50", height="2", font=("Helvetica", 13), fg="white").pack()
-Button(text="Copy+Search text", height="2", width="20", font=("Helvetica", 13), command=textg).pack()
-Button(text="Copy text", height="2", width="20", font=("Helvetica", 13), command=textp).pack()
-Button(text="Search text", height="2", width="20", font=("Helvetica", 13), command=texts).pack()
+Button(text="Copy+Search text", height="2", width="30", font=("Helvetica", 13), command=p1).pack()
+Button(text="Search text", height="2", width="30", font=("Helvetica", 13), command=p2).pack()
+Button(text="Copy text", height="2", width="30", font=("Helvetica", 13), command=p3).pack()
+Button(text="Select image(Search+Copy text)", height="2", width="30", font=("Helvetica", 13), command=s1).pack()
+Button(text="Select image(Search text)", height="2", width="30", font=("Helvetica", 13), command=s2).pack()
+Button(text="Select image(Copy text)", height="2", width="30", font=("Helvetica", 13), command=s3).pack()
 global out
 out=Text(root,width=50,height=5)
 out.pack()
