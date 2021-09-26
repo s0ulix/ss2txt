9f49295d3c02974b48dff6cb387acfac3ab31b67
Divya <sombeerjatt@gmail.com>
2021-09-26 14:39:18 +0530

    Update main(easyocr).py

--- main(easyocr).py
+++ main(easyocr).py
@@ -1,108 +1,42 @@
-import pytesseract
 import webbrowser
+import easyocr
 from PIL import ImageGrab
-from PIL import Image as IMG
 from tkinter import *
-import pyperclip
 from tkinter import messagebox
+import pyperclip
 from tkinter.filedialog import askopenfilename
-import time
-pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 def clr():
     out.delete('1.0', END)
 def textg(ch):
-    clr()
-    try:
-        if(ch==1):
-            img = ImageGrab.grabclipboard()
-            img.save('snip.png', 'PNG')
+	@@ -16,16 +19,13 @@ def textg(ch):
             file_name = "snip.png"
         else:
             file_name=ch
-        b=pytesseract.image_to_string(IMG.open(file_name))
+        output = reader.readtext(file_name)
+        b = ''
+        for a in output:
+            b += a[1]
+            b += ' '
         return b
     except Exception as ex:
         messagebox.showerror(title="Error", message="No screenshot taken or copied image")
         print(ex)
         return None
-
 def fi():
     file = askopenfilename(filetypes=[("image", ".jpeg"),("image", ".png"),("image", ".jpg"),])
     b=textg(file)
-    return b
-def cpysr(b):
-    if(b!=None):
-        out.insert(INSERT, b)
-        url = "https://www.google.com.tr/search?q={}".format(b)
-        webbrowser.open_new_tab(url)
-        pyperclip.copy(b)
-        pyperclip.paste()
-def ser(b):
-    if (b != None):
-        url = "https://www.google.com.tr/search?q={}".format(b)
-        webbrowser.open_new_tab(url)
-        out.insert(INSERT, b)
-def cpy(b):
-    if (b != None):
-        pyperclip.copy(b)
-        pyperclip.paste()
-        out.insert(INSERT, b)
-def p1():
-    b=textg(1)
-    cpysr(b)
-def p2():
-    b = textg(1)
-    ser(b)
-def p3():
-    b = textg(1)
-    cpy(b)
-def s1():
-    b=fi()
-    cpysr(b)
-def s2():
-    b=fi()
-    ser(b)
+	@@ -65,22 +65,56 @@ def s2():
 def s3():
     b=fi()
     cpy(b)
-def test(ch=1):
-    clr()
-    try:
-        if(ch==1):
-            img = ImageGrab.grabclipboard()
-            img.save('snip.png', 'PNG')
-            file_name = "snip.png"
-        else:
-            file_name=ch
-        b=pytesseract.image_to_string(IMG.open(file_name))
-        return b
-    except Exception as ex:
-        pass
-        #messagebox.showerror(title="Error", message="No screenshot taken or copied image")
-        #print(ex)
-        #return None
-def s4():
-    global isrun
-    if isrun:
-        a = test()
-        cpysr(a)
-    root.after(1000, s4)
-
-def s5():
-    global isrun
-    if isrun:
-        out.insert(INSERT, 'Auto off')
-        isrun=False
-    else:
-        out.insert(INSERT, 'Auto on')
-        isrun=True
-
+global reader
+reader = easyocr.Reader(['en'])
 root = Tk()
-#logo = PhotoImage(file='logo.png')
+logo = PhotoImage(file='logo.png')
 root.configure(bg="black")
-root.geometry("400x500")
+root.geometry("400x450")
 root.title("Text Extracter")
-#root.iconphoto(False,logo)
+root.iconphoto(False,logo)
 Label(text="By s0ulix", bg="red", width="50", height="2", font=("Helvetica", 13), fg="white").pack()
 Button(text="Copy+Search text", height="2", width="30", font=("Helvetica", 13), command=p1).pack()
 Button(text="Search text", height="2", width="30", font=("Helvetica", 13), command=p2).pack()
@@ -110,11 +44,7 @@ Button(text="Copy text", height="2", width="30", font=("Helvetica", 13), command
 Button(text="Select image(Search+Copy text)", height="2", width="30", font=("Helvetica", 13), command=s1).pack()
 Button(text="Select image(Search text)", height="2", width="30", font=("Helvetica", 13), command=s2).pack()
 Button(text="Select image(Copy text)", height="2", width="30", font=("Helvetica", 13), command=s3).pack()
-Button(text="Auto", height="2", width="30", font=("Helvetica", 13), command=s5).pack()
 global out
 out=Text(root,width=50,height=5)
 out.pack()
-global isrun
-isrun=False
-root.after(1000, s4)
 root.mainloop()
